defff037524c9ff23fc051a194534d52d8277adc
Jawad Ahmad <jawainc@gmail.com>
2017-05-12 10:42:43 +0500

    Implemented Delete for portfolio

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -43,6 +43,15 @@ class PortfoliosController < ApplicationController
     end
   end
 
+  def destroy
+    @portfolio_item = Portfolio.find(params[:id])
+
+      @portfolio_item.destroy
+      respond_to do |format|
+        format.html { redirect_to portfolios_url, notice: 'Record was deleted' }
+      end
+  end
+
   #ToDo Add methods for repeated code
 
 end
